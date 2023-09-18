db5cbe43796d3cc09756d455c4f4ac87d9eb95f1
Dheepikaray <dheepikaraytp551@gmail.com>
2023-09-18 19:55:36 +0530

    javascript

---
+++ querySelector.js
@@ -0,0 +1,8 @@
+//let heading1=document.querySelector(".head");
+//heading1.style.color='red';
+//heading1.style.background-color="green";
+
+let heading1=document.querySelectorAll('.head')
+for(let i=0;i<heading1.length;i++){
+    heading1[i].style.backgroundColor ="blue";
+}
